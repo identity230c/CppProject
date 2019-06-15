e060d4fdc623776e3048068402e54c859e3eff33
Jongsik Lee <identity230c@kookmin.ac.kr>
2019-06-15 13:23:50 +0900

    box 포지션 cpp없이

--- source/box.h
+++ source/box.h
@@ -8,25 +8,50 @@
 
 #include<iostream>
 
+int dirx[4] = { 0, 0, -1, 1 };
+
+int diry[4] = { 1, -1, 0, 0 };
+
 
 
 class Box {
 
 public:
 
-	Box(int x = 0, int y = 0, bool isChar = false);
+	Box(int x = 0, int y = 0, bool isChar = false): p(x, y), isChar(isChar)
+	{
+    	pushed = 0;
+	}
 
-	Box(Box& anti);
+	Box(Box& anti): p(anti.p), isChar(anti.isChar) 
+	{
+	    pushed = 0;
+	}
 
-	void setPosition(int x, int y);
 
-	void setPosition(Position& anti);
+	void setPosition(int x, int y){
+	    p.x = x;
+    	p.y = y;
+	    pushed++;
+	}
+
+	void setPosition(Position& anti){
+		p.x = anti.x;
+		p.y = anti.y;
+		pushed++;
+	}
 	
-	int getPushed();
+	int getPushed(){
+		return pushed;
+	}
 
-	Position getPosition();
+	Position getPosition(){
+		return p;
+	}
 
-	Position move(int vector);
+	Position move(int vector){
+	    return Position(dirx[vector] + p.x, diry[vector] + p.y);
+	}
 
 	void setIsChar(bool x) {
 
